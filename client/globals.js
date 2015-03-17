4b14a9a749ed8fea7a16ff93f322d06c8eda5387
Luca Naterop <luca.naterop@bluewin.ch>
2015-03-18 00:02:47 +0100

    Added login abstraction function
    this function does two things:
    
    1. It checks wether the user is logged in. If logged in, it returns
    true, otherwise false.
    
    2. If the user is not logged in, this function will show a login popup
    which lets the user log in.

---
+++ client/globals.js
@@ -0,0 +1,12 @@
+isLoggedIn = function(){
+
+	if(! Meteor.user()){
+		// user is not logged in, so let's show login popup
+		alert('You must login before performing this action.');
+		return false;
+	}
+
+	else
+		return true;
+
+}
--- common/collections.js
+++ common/collections.js
@@ -3,10 +3,20 @@ Messages		= new Mongo.Collection ('messages');
 Tags			= new Mongo.Collection ('tags');
 AdminShizzle	= new Mongo.Collection ('adminshizzle');
 
-var a = [
+
+tags = [
 	{tag: 'Bücher', count: 0},
 	{tag: 'Möbel', count: 0},
 	{tag: 'Unterhaltungselektronik', count: 0},
 	{tag: 'Telefon/Mobile', count: 0},
 	{tag: 'Haushaltsgeräte', count: 0},
 ]
+
+
+/*
+
+FIXTURES: 
+
+tags.forEach(function(tag){	Tags.insert(tag);   });
+
+*/
