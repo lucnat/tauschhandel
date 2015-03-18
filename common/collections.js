3a79c2377275df21f0d17771bbaf1266a1736e2b
Luca Naterop <luca.naterop@bluewin.ch>
2015-03-18 01:13:03 +0100

    Minor changes

--- common/collections.js
+++ common/collections.js
@@ -2,20 +2,3 @@ Posts			= new Mongo.Collection ('posts');
 Messages		= new Mongo.Collection ('messages');
 Tags			= new Mongo.Collection ('tags');
 AdminShizzle	= new Mongo.Collection ('adminshizzle');
-
-
-tags = [
-	{tag: 'Bücher', 					count: 0},
-	{tag: 'Möbel', 						count: 0},
-	{tag: 'Unterhaltungselektronik', 	count: 0},
-	{tag: 'Telefon/Mobile', 			count: 0},
-	{tag: 'Haushaltsgeräte', 			count: 0},
-];
-
-/*
-
-FIXTURES: 
-
-tags.forEach(function(tag){	Tags.insert(tag);   });
-
-*/
