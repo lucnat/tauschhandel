f7404535d795d1ef80d09a4f0d12d2e31b3f8150
Luca Naterop <luca.naterop@bluewin.ch>
2015-03-20 17:39:24 +0100

    Added post discussion functionality

--- client/detailView.js
+++ client/detailView.js
@@ -2,17 +2,20 @@ Template.detailView.events({
 	'submit form': function(event){
 		event.preventDefault();
 		
-		var message = {
-			subject: 	'Frage zu '+ this.title,
-			text: 		$('#text').val(),
-			absender: 	Meteor.user()._id,
-			empfaenger: this.userID,
-			sentAt: 	new Date(),
-			readAt: 	null,
+		var newDiscussionPair = {
+			question: 	$('#text').val(),
+			answer: 	'', 
+			questioner: Meteor.user()._id,
+			answered: 	false,
+			published: 	false,
+			changedAt: 	new Date(),
 		}
 
-		Messages.insert(message);
+		var post = Posts.findOne({_id: this._id})
+		post.discussion.push(newDiscussionPair);
+		Posts.update({_id: this._id}, post);
 
 		Router.go('profile');
 	},
 });
+
--- client/home.js
+++ client/home.js
@@ -30,15 +30,18 @@ Template.createPost.events({
 			}
 
 			var newPost = {
-				title: 		$('#titel').val(),
-				text: 		$('#text').val(),
-				istAngebot: $('#istAngebot')[0].checked,
-				bild: 		$('#bild').val(),
-				tags: 		tags,
-				userID: 	Meteor.user()._id,
-				userName: 	Meteor.user().username,	
-				createdAt: 	new Date(),
-				viewCount: 	0
+				title: 			$('#titel').val(),
+				text: 			$('#text').val(),
+				istAngebot: 	$('#istAngebot')[0].checked,
+				bild: 			$('#bild').val(),
+				tags: 			tags,
+				userID: 		Meteor.user()._id,
+				userName: 		Meteor.user().username,	
+				createdAt: 		new Date(),
+				viewCount: 		0,
+				discussion: 	[],
+				interessenten: 	[],
+				vergebenAn: 	null
 			}
 
 			Posts.insert(newPost);
