71e6f357c8ec6f124488e5ba7a6a14f8e720c75b
Phips <philippe.frei.spreiter@gmail.com>
2015-03-24 15:42:43 +0100

    spam folder weg

--- .meteor/local/build/programs/web.browser/app/common/collections.js
+++
@@ -1,7 +0,0 @@
-(function(){Posts			= new Mongo.Collection ('posts');
-Messages		= new Mongo.Collection ('messages');
-Tags			= new Mongo.Collection ('tags');
-AdminShizzle	= new Mongo.Collection ('adminshizzle');
-Users 			= Meteor.users;
-
-})();
