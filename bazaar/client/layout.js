48a159562b709f40dc1dd93dd1340b2cf1fc5cb2
Luca Naterop <luca.naterop@bluewin.ch>
2015-11-11 22:52:13 +0100

    Android Back button routing

--- bazaar/client/layout.js
+++ bazaar/client/layout.js
@@ -31,6 +31,26 @@ Template.layout.rendered = function(){
 			IonModal.close();
 			event.preventDefault();
 			event.stopPropagation();
+		} else if($('body').hasClass('action-sheet-open')){
+			IonActionSheet.close();
+		} else if($('.back-button').length > 0){
+			//means there is a back-button, let's press it
+			$('.back-button').click()
+		} else if(! $($('.tab-item')[0]).hasClass('active') ){
+			// means we are not in home tab, lets go there
+			$($('.tab-item')[0]).click();
+		} else if($('body').hasClass('popup-open')){
+			// lets leave it open and do nothing
+		} else {
+	        IonPopup.confirm({
+	            title: 'Basaar App schliessen?',
+	            template: '',
+	            onOk: function() {
+					navigator.app.exitApp();	            },
+	            onCancel: function() {
+
+	            }
+	        });
 		}
 	}
 }
